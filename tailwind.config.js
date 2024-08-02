bf34433cadd1a085cda2903131a50be770940c48
AndrewAlllDay <andrewsnyderdesign@gmail.com>
2024-08-02 07:34:17 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        'green': 'rgb(113, 197, 98)',
+        'greenLight': 'rgb(113, 224, 164)',
+      }
+    },
+  },
+  plugins: [require("tailgrids/plugin")], 
+  plugins: [require("daisyui")],
+}
+
